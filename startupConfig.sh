946df52adcf95dc3c3f6e213fab218ab6eced9dd
Sam Rendall <sdrendall@gmail.com>
2014-03-18 11:06:32 -0400

    added sublime and nautilus-open-terminal.  Streamlined repo addition

--- startupConfig.sh
+++ startupConfig.sh
@@ -5,10 +5,16 @@
 # James McNamara
 
 # Repos ect
+sudo add-apt-repository ppa:webupd8team/sublime-text-2 -y # sublime
+sudo apt-add-repository ppa:ehoover/compholio -y # netflix
+
 sudo apt-get update -y && apt-get upgrade -y
 
 # Install some useful programs
-sudo apt-get install terminator openssh-server nmap vim -y
+sudo apt-get install terminator openssh-server nmap vim git nautilus-open-terminal -y
+
+# Install Sublime
+sudo apt-get install sublime-text -y 
 
 # Install Spotify
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59 && sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" >> /etc/apt/sources.list' && sudo apt-get update && sudo apt-get install spotify-client -y
@@ -17,8 +23,6 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59 && sudo s
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome*; sudo apt-get -f install -y
 
 #Netflix
-sudo apt-add-repository ppa:ehoover/compholio -y &&
-sudo apt-get update -y &&
 sudo apt-get install netflix-desktop -y
 
 #Dropbox
